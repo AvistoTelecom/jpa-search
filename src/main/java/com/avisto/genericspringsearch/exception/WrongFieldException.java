363c01eae72aee863f62ecf69d0c996452f7647b
revelli <gabriel.revelli@avisto.com>
2023-08-28 11:24:59 +0200

    :bug: (superclass collection exception): Remove unused exception

--- src/main/java/com/avisto/genericspringsearch/exception/WrongFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.avisto.genericspringsearch.exception;
-
-public class WrongFieldException extends GenericSearchException {
-    public WrongFieldException(String message) {
-        super(message);
-    }
-}
