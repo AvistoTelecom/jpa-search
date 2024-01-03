c0f9e047352c2ea5d80d7afb3bbaf9eda4dea74c
revelli <gabriel.revelli@avisto.com>
2024-01-03 18:21:48 +0100

    :bug: Fix tests

--- src/main/java/com/avisto/genericspringsearch/config/FilterConfig.java
+++ src/main/java/com/avisto/genericspringsearch/config/FilterConfig.java
@@ -58,7 +58,7 @@ public class FilterConfig<R extends SearchableEntity, T> implements IFilterConfi
         if (SearchUtils.isPrimitiveType(entryClazz)) {
             sanitizeEntryClazz = SearchUtils.getObjectTypeFromPrimitiveType(entryClazz);
         }
-        if (sanitizeEntryClazz != Void.class && !filterOperation.getOperationType().isAssignableFrom(sanitizeEntryClazz)) {
+        if (filterOperation.getOperationType() != Void.class && !filterOperation.getOperationType().isAssignableFrom(sanitizeEntryClazz)) {
             throw new FilterOperationException(String.format("Filter Operation with operation type %s cannot be assigned to %s", filterOperation.getOperationType(), entryClazz));
         }
         if (paths.stream().anyMatch(path -> SearchUtils.getEntityClass(rootClazz, path.split(REGEX_DOT)) != targetClazz)) {
