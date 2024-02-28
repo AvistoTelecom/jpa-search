03900a4d12f8cc33088ac9a5e6462b62c7b2129b
UF814MRE <martin.rech@avisto.com>
2024-02-28 10:50:46 +0100

    [+] fix tests not pass

--- src/test/java/com/avisto/genericspringsearch/model/TestNested.java
+++ src/test/java/com/avisto/genericspringsearch/model/TestNested.java
@@ -1,5 +1,11 @@
 package com.avisto.genericspringsearch.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class TestNested {
+    @Id
+    private Long id;
     private Integer nestedField;
 }
