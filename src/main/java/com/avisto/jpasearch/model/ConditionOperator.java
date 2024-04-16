8ae078326c4fc7aebaf5a5f027e3b60a64c0c48f
revelli <gabriel.revelli@avisto.com>
2024-04-16 13:09:39 +0200

    Jakarta imports to javax

--- src/main/java/com/avisto/jpasearch/model/ConditionOperator.java
+++ src/main/java/com/avisto/jpasearch/model/ConditionOperator.java
@@ -1,7 +1,7 @@
 package com.avisto.jpasearch.model;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.Predicate;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
 
 import java.util.function.BiFunction;
 
